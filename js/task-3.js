e6e76913fb4455bef300d89d8a36debc68cb09a9
ivankr0711 <160317804+ivankr0711@users.noreply.github.com>
2024-04-29 16:42:22 +0300

    1

--- js/task-3.js
+++ js/task-3.js
@@ -5,4 +5,8 @@ function getElementWidth (content, padding, border){
     const total_width = content_width + padding_width + border_width;
     return total_width;
 }
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px"));
+
 
